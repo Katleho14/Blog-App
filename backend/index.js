c1e2ec59f38e5af1718ab66518812e2c8a6b1031
Katleho14 <katlehomotloung394@gmail.com>
2025-03-07 09:22:10 +0200

    feat: update database connection in backend index file and add custom word to cSpell settings

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // database connection
-    const connectDB= async () => {
-        try {
-            await mongoose.connect()
-    }
+const connectDB=async()=>{
+    try{
+        await mongoose.connect("mongodb+srv://username123:username123@cluster0.5hvdx.mongodb.net/BlogApp?retryWrites=true&w=majority");
 
-    catch (err) {
-    console.log(err);
-    }
 
+    }
+    catch(err){
+        console.log(err)
+    }
 }
+
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
