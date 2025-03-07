3b10b83365c7b9c49c18141bb7825e1d9a066ed8
Katleho14 <katlehomotloung394@gmail.com>
2025-03-07 13:21:19 +0200

    feat: update database connection string and change server port to 5002 in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const dotenv = require('dotenv');
 
 // database connection
 const connectDB=async()=>{
     try{
-        await mongoose.connect()
+        await mongoose.connect("mongodb+srv://username123:<username123>@cluster0.5hvdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("database is connected successfully!")
 
     }
@@ -16,7 +15,6 @@ const connectDB=async()=>{
 }
 
 
-connectDB()
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(5002, () => {
+    console.log('Server is running on port 5002');
     })
