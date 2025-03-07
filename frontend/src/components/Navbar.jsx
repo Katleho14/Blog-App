8550db109f8906f083926d16bae61da6777c1292
Katleho14 <katlehomotloung394@gmail.com>
2025-03-07 02:55:08 +0200

    feat: implement user-based navigation in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { BsSearch } from 'react-icons/bs'
 
 const Navbar = () => {
+  const user=true
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
 <h1 className="text-xl font-extrabold"><Link to ="/">Squirrel Blog</Link></h1>
@@ -10,8 +11,8 @@ const Navbar = () => {
 <input type="text" placeholder="Search a post" className="border-none focus:outline-none px-3"/>
 </div>
 <div className="flex items-center justify-center space-x-2 md:space-x-4">
-<h3><Link to ="/">Login</Link></h3>
-<h3><Link to ="/">Register</Link></h3>
+{user? <h3><Link to="/write">Write</Link></h3> :<h3><Link to="/login">Login</Link></h3>}
+{user? <h3>Profile</h3>:<h3><Link to ="/">Register</Link></h3>}
 </div>
 
     </div>
