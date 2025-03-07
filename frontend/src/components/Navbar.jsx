d1c351fa826c77cb72d8c144a02463fb8f977c3e
Katleho14 <katlehomotloung394@gmail.com>
2025-03-07 02:41:39 +0200

    fix: update Navbar title to 'Squirrel Blog'

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { BsSearch } from 'react-icons/bs'
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-<h1 className="text-xl font-extrabold"><Link to ="/">Blog</Link></h1>
+<h1 className="text-xl font-extrabold"><Link to ="/">Squirrel Blog</Link></h1>
 <div className="flex justify-center items-center space-x-0">
 <p><BsSearch/></p>
 <input type="text" placeholder="Search a post" className="border-none focus:outline-none px-3"/>
